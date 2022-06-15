974f79ed9cf5f1b5d042c2320264f32c96dc49a2
davemolk <davemolk@gmail.com>
2022-06-15 05:24:11 -0600

    add user model

--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require (
 )
 
 require golang.org/x/time v0.0.0-20220609170525-579cf78fd858
+
+require golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
--- internal/data/models.go
+++ internal/data/models.go
@@ -12,10 +12,12 @@ var (
 
 type Models struct {
 	Albums AlbumModel
+	Users UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Albums: AlbumModel{DB: db},
+		Users: UserModel{DB: db},
 	}
 }
