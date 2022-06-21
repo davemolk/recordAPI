42f01f71603b48098ff4ea15b31a3fc6a5845b60
davemolk <davemolk@gmail.com>
2022-06-21 05:23:53 -0600

    user activation

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,12 +12,14 @@ var (
 
 type Models struct {
 	Albums AlbumModel
+	Tokens TokenModel
 	Users UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Albums: AlbumModel{DB: db},
+		Tokens: TokenModel{DB: db},
 		Users: UserModel{DB: db},
 	}
 }
